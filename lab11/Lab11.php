e2e4acbbdb4a16c498f0f4346a074699021b29d9
gta0804 <18302010041@fudan.edu.cn>
2019-06-06 11:25:48 +0800

    commit for lab11

--- lab11/Lab11.php
+++ lab11/Lab11.php
@@ -37,11 +37,17 @@
         <div class="panel panel-default">
           <div class="panel-heading">Filters</div>
           <div class="panel-body">
-            <form action="Lab11.php" method="get" class="form-horizontal">
+            <form action="Lab11.php" method="post" class="form-horizontal">
               <div class="form-inline">
               <select name="continent" class="form-control">
                 <option value="0">Select Continent</option>
                 <?php
+                require_once("login.php");
+                $conn= new mysqli($hn,$un,$pw,$db);
+                if($conn->connect_error) die($conn->connect_error);
+                $query="SELECT * FROM continents";
+                $result = $conn->query($query);
+
                 //Fill this place
 
                 //****** Hint ******
@@ -59,6 +65,18 @@
                 <?php 
                 //Fill this place
 
+                $query="SELECT * FROM countries";
+                $result = $conn->query($query);
+
+                //Fill this place
+
+                //****** Hint ******
+                //display the list of continents
+
+                while($row = $result->fetch_assoc()) {
+                    echo '<option value=' . $row['ISO'] . '>' . $row['CountryName'] . '</option>';
+                }
+
                 //****** Hint ******
                 /* display list of countries */ 
                 ?>
@@ -73,8 +91,46 @@
                                     
 
 		<ul class="caption-style-2">
-            <?php 
-            //Fill this place
+            <?php
+            $conn= new mysqli($hn,$un,$pw,$db);
+            $query="SELECT * FROM imagedetails";
+            $continent=get_post($conn,"continent");
+            $country=get_post($conn,"country");
+            if($continent=="0"&&$country=="0"){
+                $query="SELECT * FROM imagedetails";
+            }
+            else if($continent=="0"){
+                $query ="SELECT * FROM imagedetails WHERE CountryCodeISO='$country'";
+            }
+            else if($country=="0"){
+                $query ="SELECT * FROM imagedetails WHERE ContinentCode='$continent'";
+            }
+            else{
+                $query ="SELECT * FROM imagedetails WHERE ContinentCode='$continent' AND CountryCodeISO='$country'";
+            }
+            $result = $conn->query($query);
+            $rows =$result->num_rows;
+            for($j=0;$j<$rows;++$j){
+                $result->data_seek($j);
+                $row=$result->fetch_array(MYSQLI_ASSOC);
+                $imageID=$row["ImageID"];
+                $imagePath=$row["Path"];
+                $imageDescription=$row["Description"];
+                echo '<li>';
+                echo "<a href='details.php?id=$imageID' class='img-responsive'>";
+                echo "<img src='images/square-medium/$imagePath' alt='$imageDescription'>";
+                echo "<div class='caption'>";
+                echo "<div class='blur'></div>";
+                echo "<div class='caption-text'>";
+                echo "<p>$imageDescription</p>";
+                echo "</div>";
+                echo "</div>";
+                echo "</a>";
+                echo '</li>';
+
+
+            }
+                //Fill this place
 
             //****** Hint ******
             /* use while loop to display images that meet requirements ... sample below ... replace ???? with field data
@@ -89,7 +145,15 @@
                 </div>
               </a>
             </li>        
-            */ 
+            */
+
+            function findByContinentsAndCountries($query,$conn){
+
+            }
+
+            function get_post($conn,$var){
+                return $conn->real_escape_string($_POST[$var]);
+            }
             ?>
        </ul>       
 
---
+++ lab11/login.php
@@ -0,0 +1,5 @@
+<?php
+$hn="localhost";
+$un="root";
+$pw="gta0804,.";
+$db="travel";
